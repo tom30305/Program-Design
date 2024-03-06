092b61b406ca52f13ee8056a368d0da70527ea45
Amo <158478742+tom30305@users.noreply.github.com>
2024-03-06 18:48:19 +0800

    Add files via upload

---
+++ HW31/HW3_Q2.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(void)
+{
+	int a, b;
+	printf("輸入a、b兩個整數:");
+	scanf_s("%d %d", &a, &b);
+	printf("a+b=%d\n",a+b);
+	system("pause");
+	return 0;
+}
